2617ac846ac2cc065109c3c917187069620cff3d
VijayaPrashanth <vijaya.prashanth500@gmail.com>
2023-09-01 13:09:53 +0530

    Add. test for footer

---
+++ src/components/footer/Footer.spec.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+
+import { Typography } from "@material-ui/core";
+import Footer from "./Footer";
+
+ configure({ adapter: new Adapter() });
+describe("Basic rendering", () => {
+   
+
+  it("Should display version text", () => {
+    const footerComponent = shallow(
+      <Footer/>
+    );
+    const typographyComponent = footerComponent.find(Typography);
+    const version = typographyComponent.at(0);
+    expect(typographyComponent.length).toBe(1);
+    expect(version.text()).toBe("Version : ");
+  });
+
+  
+});
