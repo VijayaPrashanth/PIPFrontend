a3c056d23877e16ac8ab2981126f9a82ed89ce5d
VijayaPrashanth <vijaya.prashanth500@gmail.com>
2023-09-01 12:30:49 +0530

    Refactor. implemented priceList page rendering and the tests are passed

---
+++ src/components/priceList/PriceList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Typography } from "@material-ui/core";
+
+
+const PriceList=()=>{
+    return(
+        <>
+            <Typography variant="h5">
+                PriceList
+            </Typography>
+        </>
+    );
+};
+
+export default PriceList;
