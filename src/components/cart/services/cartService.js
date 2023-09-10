864d820c0eee0c9e2f2ab9aa08eece32c065010c
VijayaPrashanth <vijaya.prashanth500@gmail.com>
2023-09-10 09:42:54 +0530

    Add. service for cart

---
+++ src/components/cart/services/cartService.js
@@ -0,0 +1,17 @@
+import apiService from "../../helpers/apiService"
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    getItemsFromCart: async () => {
+        return await apiService.get('cart');
+    },
+    updateCart: async (payload) => {
+        return await apiService.put('cart', payload);
+    },
+    deleteItemFromCart: async (id) => {
+        return await apiService.delete('cart', id);
+    },
+    deleteItemFromCartByInventory: async (id) => {
+        return await apiService.delete('cart/byInventory', id);
+    }
+}
