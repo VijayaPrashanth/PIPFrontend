fdf1d096c93d3ce4759ab62f80c9268618e584fe
VijayaPrashanth <vijaya.prashanth500@gmail.com>
2023-09-10 20:02:55 +0530

    Refactor. added test for updateCart apicall

--- src/components/helpers/apiService.spec.js
+++ src/components/helpers/apiService.spec.js
@@ -1,3 +1,4 @@
+/* eslint-disable jest/no-conditional-expect */
 import axios from "axios";
 import apiService from "./apiService";
 import AxiosMockAdapter from "axios-mock-adapter";
@@ -10,38 +11,38 @@ describe('Api service', () => {
         mock.reset();
     });
     
-    it("should make a GET request to version path", async () => {
+    it("should make a GET request to specified path", async () => {
         const path = "version";
         const responseData = {"CurrentVersion":"v1"};
 
         mock.onGet(`http://localhost:8080/${path}`).reply(200, responseData);
-
-        const response = await apiService.version('version');
+        const response = await apiService.get('version');
 
         expect(response.status).toBe(200);
         expect(response.data).toEqual(responseData);
     });
 
     it("should make a POST request to the specified path", async () => {
-        const path = "cart/add";
+        const path = "inventory";
         const responseData = { "id": 1,"name":"onion","quantity":2 };
-
         const payload = {"id":1,"name":"onion","quantity":2}
-        mock.onPut(`http://localhost:8080/${path}`,payload).reply(200, responseData);
 
-        const response = await apiService.add('cart/add',payload);
+        mock.onPost(`http://localhost:8080/${path}`,payload).reply(200, responseData);
+        const response = await apiService.post('inventory',payload);
 
         expect(response.status).toBe(200);
         expect(response.data).toEqual(responseData);
     });
 
-    it("should make a GET request to cart path", async () => {
-        const path = "cart";
-        mock.onGet(`http://localhost:8080/${path}`).reply(200);
+    it("should make a PUT request to cart path", async () => {
+        const path = "inventory";
+        const payload = { "id": 1, "name": "onion", "quantity": 2 }
+        mock.onPut(`http://localhost:8080/${path}`,payload).reply(200,payload);
 
-        const response = await apiService.getCart('cart');
+        const response = await apiService.put('inventory',payload);
 
         expect(response.status).toBe(200);
+        expect(response.data).toEqual(payload);
     });
 
     it("should make a DETELE request to cart path", async () => {
@@ -49,10 +50,40 @@ describe('Api service', () => {
         const id = 1;
         mock.onDelete(`http://localhost:8080/${path}/${id}`).reply(200);
 
-        const response = await apiService.deleteItem('cart',1);
+        const response = await apiService.delete('cart',1);
 
         expect(response.status).toBe(200);
     });
+    it('should Update Cart Item By Id', async () => {
+        const path = 'cart';
+        const id = '1';
+        const payload = { quantity: 2};
 
+        mock.onPut(`http://localhost:8080/${path}/${id}`).reply(200, { message: 'Cart item updated' });
+        const response = await apiService.updateCartItem(path, id, payload);
+
+        expect(response.status).toBe(200);
+        expect(response.data.message).toBe('Cart item updated');
+    });
+
+    it('should handle 500 internal server error', async () => {
+        mock.onAny().reply(500);
+        try {
+            await apiService.get('cart');
+        } catch (error) {
+            expect(error.response.status).toBe(500);
+            expect(window.location.assign).toHaveBeenCalledWith('/error');
+        }
+    });
+
+    it('should handle other errors', async () => {
+        mock.onAny().reply(404, { message: 'Not Found' });
+
+        try {
+            await apiService.get('inventory');
+        } catch (error) {
+            expect(error.response.status).toBe(404);
+        }
+    });
 
 });
